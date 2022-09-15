b0ad01c3c2c19d08d2f88b818659a3626ad97360
j0ey <j0ey_huang@163.com>
2022-09-15 22:54:48 +0800

    ready for ch1

--- src/lang_items.rs
+++ src/lang_items.rs
@@ -1,6 +1,17 @@
 use core::panic::PanicInfo;
+use crate::sbi::shutdown;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
+fn panic(info: &PanicInfo) -> ! {
+    if let Some(location) = info.location() {
+        println!("[Kernel] Panicked at {}:{} {}",
+                location.file(),
+                location.line(),
+                info.message().unwrap()
+            );
+    }
+    else {
+        println!("[Kernel] Panicked at {}", info.message().unwrap());
+    }
+    shutdown()
 }
